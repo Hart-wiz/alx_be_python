ebfac80233860bc78e793bae448719de3a34d702
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-01 07:35:05 +0100

    proper variable assignment

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
  
-P = 1000
-R = 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-I = P * R * T
+interest = principal * rate * time
 
 
-print ("The simple interest is: "+ str(I))
+print ("The simple interest is: "+ str(interest))
