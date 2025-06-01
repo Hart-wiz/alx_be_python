640cecaee050c5eca70f826279cb6d7f4df3acff
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-01 07:18:04 +0100

    basic_operations done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+
+number1 = 10
+number2 = 5
+
+addition = "Addition of " + str(number1) + " and " + str(number2) + " is " + str(number1 + number2)
+subtraction = "subtraction of " + str(number1) + " and " + str(number2) + " is " + str(number1 - number2)
+multiplication = "multiplication of " + str(number1) + " and " + str(number2) + " is " + str(number1 * number2)
+
+print(addition)
+print(subtraction)
+print(multiplication)
